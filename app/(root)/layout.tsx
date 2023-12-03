cfe5c17ab7a640022b606695f258cbd0d0a37846
Wilson Chan <wilsonchan54@gmail.com>
2023-12-03 16:06:19 -0500

    Created color themes in theme.js and global.css / added topbar and leftsidebar components to root layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../globals.css'
+import Topbar from '@/components/global/Topbar'
+import LeftSidebar from '@/components/global/LeftSidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        <Topbar />
+
+        <main>
+
+          <LeftSidebar />
+          
+          <section>
+            {children}
+          </section>
+
+        </main>
+
+      </body>
     </html>
   )
 }
